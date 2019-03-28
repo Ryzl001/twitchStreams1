8950e22b32fd5bc4f83c4a581b543fbecdf70133
Stan <potomak123@googlemail.com>
2019-03-28 13:16:28 +0100

    Updating Auth State

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -13,10 +13,17 @@ class GoogleAuth extends Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
+
+          // zamiast tej lini możemy wstawić this.onAuthChange() - robi to samo, zmienia state na aktualny
           this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
         });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
