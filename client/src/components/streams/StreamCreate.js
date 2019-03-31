08d5866cae72fa5b6072cfc1e62eed050d9a3e7f
Stan <potomak123@googlemail.com>
2019-03-31 21:48:10 +0200

    Showing errors on Touch

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,18 +3,31 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
+  /** Semating.ui domyślnie error message ustawia display: none w CSS */
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
   // Wyciągamy z formProps tylko { input }
   // Wyciągamy kolejne właściwości, które zostały przekazane do pola Field
-  renderInput({ input, label, meta }) {
+  renderInput = ({ input, label, meta }) => {
+    const classNameError = `field ${meta.error && meta.touched ? "error" : ""}`;
     return (
       // wypluwamy wszystkie właściwości formProps.input do <input />, takie jak onChange, value itp
-      <div className="field">
+      /** żeby podświtlić inputa w formularzu wystarczy dodać klasę error className="field error" */
+
+      <div className={classNameError}>
         <label>{label}</label>
-        <input {...input} />
-        <div>{meta.error}</div>
+        <input {...input} autoComplete="off" />
+        <div>{this.renderError(meta)}</div>
       </div>
     );
-  }
+  };
   // zamiast event wstawiamy formValue
   onSubmit(formValues) {
     // event.preventDefault(); - redux-form zrobi to za nas, nie musimy tego wstawiać
@@ -24,9 +37,12 @@ class StreamCreate extends Component {
   render() {
     return (
       // w Field przekazujemy właściwości do renderInput, np label
+      /** do formularza trzeba dodać klasę error, żeby komunikaty o błędzie
+       * były wyświetlane prawidłowo- Sematnic UI
+       */
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
