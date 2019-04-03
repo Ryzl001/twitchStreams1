526a7d25999f67d9358df854da92e3933cd1be93
Stan <potomak123@googlemail.com>
2019-04-04 00:09:25 +0200

    Rendering All Streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -7,12 +7,40 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderStreamList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="ui raised segment item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>StreamList</div>;
+    console.log(this.props);
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreamList()}</div>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = state => {
+  /**Object.values jako argument bierze object
+   * i zwraca tablicę z values tego obiektu
+   * używamy, aby potem użyć map na tablicy do wyrenderowania jej zawartości
+   */
+  return { streams: Object.values(state.streams) };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
