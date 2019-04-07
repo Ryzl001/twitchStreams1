b4e1f31ba66fcd0c3b8dbee04f7c63239bcd5a79
Stan <potomak123@googlemail.com>
2019-04-07 17:26:25 +0200

    Creating a Portal

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import Modal from "../Modal";
 
 class StreamDelete extends Component {
   render() {
     return (
       <div>
-        <div>Stream Delete</div>
-        <Link to="/">home</Link>
+        <div>
+          Stream Delete
+          <Modal />
+        </div>
       </div>
     );
   }
